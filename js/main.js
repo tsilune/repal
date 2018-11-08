e722a0de991a6280f62e21440cf6d1b2e863154a
Tsi <tsilune@gmail.com>
2018-11-08 11:00:58 +0100

    yes

--- js/main.js
+++ js/main.js
@@ -3,14 +3,25 @@ var ripple = document.querySelector('.ripple');
 
 // au clic sur le ripple, permuter la classe .visible sur le container
 // l'enlever si elle est présente, l'ajouter sinon
-container.addEventListener('click', function() {
+container.addEventListener('click', function(e) {
 	ripple.classList.toggle('active');
+
+	var rect = e.target.getBoundingClientRect();
+	var x = e.clientX - rect.left; //x position within the element.
+	var y = e.clientY - rect.top;
+
+	ripple.style.top = y + 'px';
+	ripple.style.left = x + 'px';
+
+	setTimeout(function() {
+		ripple.classList.toggle('active');
+	}, 500);
 });
 
 
 
 
 // e = Mouse click event.
-var rect = e.target.getBoundingClientRect();
-var x = e.clientX - rect.left; //x position within the element.
-var y = e.clientY - rect.top; //y position within the element.
+// var rect = e.target.getBoundingClientRect();
+// var x = e.clientX - rect.left; //x position within the element.
+// var y = e.clientY - rect.top; //y position within the element.
